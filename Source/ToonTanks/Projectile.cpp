bbd5b2b71b4be2e269a87b09bb3fd756f5dff8b3
Mustafa_yagya <61277144+Mustyagya@users.noreply.github.com>
2024-06-29 14:34:57 +0300

    Desrtoy projectile
    это если даже что-то запороли так все равно снаряд уничтожится

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -55,10 +55,10 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimi
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
 
 		UGameplayStatics::SpawnEmitterAtLocation(this,HitParticles,GetActorLocation(),GetActorRotation());
-		// Уничтожаем снаряд после столкновения
-		Destroy();
+		
 	}
-
+	// Уничтожаем снаряд после столкновения
+	Destroy();
  
 }
 
